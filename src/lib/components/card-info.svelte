e2d15efaa0a4a335fbb2b742a5f0c97a28f52c74
hucancode <hucancode@gmail.com>
2023-02-10 12:39:48 +0900

    fix safari grid bug

--- src/lib/components/card-info.svelte
+++ src/lib/components/card-info.svelte
@@ -3,7 +3,7 @@
   import IconUp from "~icons/ph/caret-double-up-duotone";
   import IconDown from "~icons/ph/caret-double-down-duotone";
   export let card;
-  export let revealed = false;
+  export let revealed = true;
   export let upright = true;
   export let reverse = true;
 </script>
@@ -13,16 +13,16 @@
     class="flex grow flex-col items-center border border-gray-600 bg-gray-100 p-4 text-green-800 dark:bg-gray-800 dark:text-green-400"
   >
     <IconUp />
-    <div class="grid h-full w-full grow place-items-center text-center">
-      {#if revealed}
-        <div transition:fade class:dim={!upright}>
+    <div class="flex items-center h-full w-full grow text-center">
+      {#if revealed || true}
+        <div transition:fade class:dim={!upright} class="w-full h-max">
           {card.meaning.upright.join(", ")}
         </div>
       {/if}
     </div>
   </div>
   <h3 class="m-0 px-2 py-4 text-center text-3xl font-bold uppercase">
-    {#if revealed}
+    {#if revealed || true}
       <div transition:fade>
         {card.name}
         {upright && !reverse
@@ -38,9 +38,9 @@
   <div
     class="flex grow flex-col items-center border border-gray-600 bg-gray-100 p-4 text-blue-800 dark:bg-gray-800 dark:text-blue-400"
   >
-    <div class="grid h-full w-full grow place-items-center text-center">
-      {#if revealed}
-        <div transition:fade class:dim={!reverse}>
+    <div class="flex items-center h-full w-full grow text-center">
+      {#if revealed || true}
+        <div transition:fade class:dim={!reverse} class="w-full h-max">
           {card.meaning.reverse.join(", ")}
         </div>
       {/if}
